b9cf9d4d296fc39c4817cc13408f7eac2bde57da
Maury Alberto <mauryalberto209@gmail.com>
2025-06-20 10:28:35 -0700

    Added override to SavingsAccount member functions

--- include/SavingsAccount.hpp
+++ include/SavingsAccount.hpp
@@ -9,7 +9,11 @@ private:
 public:
     SavingsAccount(int accNum, const std::string& name, double balance, double rate) : Account(accNum, name, balance), interestRate(rate) {};
 
-    void applyInterest();
-    void display() const override;
+    virtual void deposit(double amount) override;
+    virtual void withdraw(double amount) override;
+    virtual void display() const override;
+    virtual void saveToFile(std::ofstream& out) const override;
+    virtual void loadFromFile(std::ifstream& in) override;
 
+    void applyInterest();
 };
---
+++ src/SavingsAccount.cpp
@@ -0,0 +1,22 @@
+#include "SavingsAccount.hpp"
+
+
+void SavingsAccount::deposit(double amount) {
+
+}
+
+void SavingsAccount::withdraw(double amount) {
+
+}
+
+void SavingsAccount::display() const {
+
+}
+
+void SavingsAccount::saveToFile(std::ofstream& out) const {
+
+}
+
+void SavingsAccount::loadFromFile(std::ifstream& in) {
+
+}
