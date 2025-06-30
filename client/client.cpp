c8d3ffe8bc29e6148b0b433a3d6044c4458c86bd
Maury Alberto <mauryalberto209@gmail.com>
2025-06-30 06:32:44 -0700

    client.cpp from last commit

---
+++ client/client.cpp
@@ -0,0 +1,46 @@
+#include "../include/Network.hpp"
+#include <arpa/inet.h>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+int main(){
+    struct sockaddr_in server_addr;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sock == -1){
+        std::perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    std::memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family= AF_INET;
+    server_addr.sin_port = htons(PORT);
+    
+    if(inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0){
+        std::cerr << "Invalid address.\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    if(connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
+        std::perror("connect");
+        exit(EXIT_FAILURE);
+    }
+
+    json req;
+    req["action"] = "CREATE";
+    req["accountNumber"] = 2000;
+    req["holderName"] = "Maury Alberto";
+    req["balance"] = 2000.00;
+    req["accountType"] = "SAVINGS";
+    req["interestRate"] = 0.02;
+
+    sendMessage(sock, req.dump());
+
+    std::string response = recieveMessage(sock);
+    std::cout << response;
+
+    close(sock);
+    return 0;
+}
